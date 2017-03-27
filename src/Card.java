076f78ccf029d1f4fc7ea334db7e342ef7c68f12
Jack David Holland <hollandjd@dcsdk12.org>
2017-03-27 12:29:45 -0600

    Activity 1

--- src/Card.java
+++ src/Card.java
@@ -32,7 +32,10 @@ public class Card {
 	 *                  containing the point value of the card
 	 */
 	public Card(String cardRank, String cardSuit, int cardPointValue) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		rank = cardRank;
+		suit = cardSuit;
+		pointValue = cardPointValue;
+		
 	}
 
 
@@ -41,7 +44,7 @@ public class Card {
 	 * @return this <code>Card's</code> suit.
 	 */
 	public String suit() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return suit;
    }
 
 	/**
@@ -49,7 +52,7 @@ public class Card {
 	 * @return this <code>Card's</code> rank.
 	 */
 	public String rank() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return rank;
 	}
 
    /**
@@ -57,7 +60,7 @@ public class Card {
 	 * @return this <code>Card's</code> point value.
 	 */
 	public int pointValue() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return pointValue;
 	}
 
 	/** Compare this card with the argument.
@@ -67,7 +70,7 @@ public class Card {
 	 *         false otherwise.
 	 */
 	public boolean matches(Card otherCard) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return (rank == otherCard.rank() && suit == otherCard.suit() && pointValue == otherCard.pointValue()) ;
 	}
 
 	/**
@@ -82,6 +85,6 @@ public class Card {
 	 */
 	@Override
 	public String toString() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return rank + " of " + suit + " (point value = " + pointValue + ")";
 	}
 }
--- src/CardTester.java
+++ src/CardTester.java
@@ -8,6 +8,12 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card card1 = new Card("3", "Hearts", 3);
+		Card card2 = new Card("3", "Hearts", 3);
+		Card card3 = new Card("Jack", "Diamonds", 11);
+		System.out.println(card1.rank());
+		System.out.println(card2.suit());
+		System.out.println(card3.pointValue());
+		System.out.println(card1.matches(card2));
 	}
 }
